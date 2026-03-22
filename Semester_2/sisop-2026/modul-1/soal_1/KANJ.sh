bedbee0eca292e29a083375fdb72bca286fb8f8b
Lunatic-Yui <yoviprayudya62@gmail.com>
2026-03-22 19:20:17 +0700

    push all

---
+++ Semester_2/sisop-2026/modul-1/soal_1/KANJ.sh
@@ -0,0 +1,76 @@
+BEGIN {
+	FS = ","
+	pilihan = tolower(ARGV[2])
+	delete ARGV[2]
+}
+
+{
+	if (NR == 1) {
+		next
+	}
+
+	#removing \r on csv
+	sub("\r", "", $0)
+
+	if (pilihan == "a") count_passenger++ 
+	if (pilihan == "b") {
+		if(!($4 in Gerbong)) {
+			carriage++
+			Gerbong[$4] = 1
+		}
+	}
+	if (pilihan == "c") {
+		if(NR == 2) {
+			age = $2
+			oldest = $1
+		} else if ($2 > age) {
+			age = $2
+			oldest = $1
+		}
+	}
+	if (pilihan == "d") {
+		total += $2
+		data++
+	}
+	if (pilihan == "e") {
+		if ($3 == "Business") {
+			business_passenger++
+		}
+	}
+}
+
+END {
+	switch (pilihan) {
+		case "a":
+		print "Jumlah seluruh penumpang KANJ adalah " count_passenger " orang"
+		break
+		case "b":
+		if (carriage == "") {
+			print "Jumlah gerbong penumpang KANJ adalah 0"
+			
+		} else {
+			print "Jumlah gerbong penumpang KANJ adalah " carriage
+		}
+		break
+
+		case "c":
+		print oldest " adalah penumpang kereta tertua dengan usia " age " tahun"
+		break
+
+		case "d":
+		if (data > 0) {
+			average_age = total / data
+			printf "Rata-rata usia penumpang adalah %.0f tahun", average_age
+		}
+		break
+
+		case "e":
+		if (business_passenger == "") business_passenger = 0
+		print "Jumlah penumpang business class ada " business_passenger " orang"
+		break
+
+		default:
+		print "Soal tidak dikenali. Gunakan a, b, c, d, atau e. \nContoh penggunaan: awk -f file.sh data.csv a"
+		break
+	}
+}
---
+++ Semester_2/sisop-2026/modul-1/soal_3/kost_slebew.sh
@@ -0,0 +1,287 @@
+#!/bin/bash
+
+re='^[0-9]+$'
+
+cd "$(dirname "$0")" 
+mkdir -p data log rekap sampah 
+if [ ! -f data/penghuni.csv ]; then
+    echo "Nama,Kamar,Harga,Tanggal,Status" > data/penghuni.csv
+fi
+
+create() {
+
+    echo "==================================================== "
+    echo "             『Daftar Huni kost Slebew 』            "
+    echo "==================================================== "
+
+    while true; do
+    read -p "Silahkan masukkan nama: " nama
+    if grep -q -w "$nama" data/penghuni.csv 2>/dev/null; then
+        echo "Maaf, Penghuni dengan nama '$nama' sudah terdaftar!"
+        echo "Silakan coba masukkan nama lain."
+        continue
+    fi
+
+    read -p "Masukkan kamar yang diinginkan: " kamar
+    if [[ $kamar =~ $re ]] ; then
+        if grep -q -w "$kamar" data/penghuni.csv 2>/dev/null; then
+            echo "Maaf, Kamar $kamar sudah terisi!"
+            echo "Silakan coba masukkan nomor kamar lain."
+            continue
+        fi
+    else
+        echo "maaf harus input angka"
+        sleep 1
+        continue
+    fi
+    read -p "Harga sewa: " price
+    read -p "Tanggal masuk (YYYY-MM-DD): " date
+    read -p "status awal (aktif atau menunggak): " status
+    if [[ "$status" == "aktif" || "$status" == "menunggak" ]]; then
+          echo "$nama,$kamar,$price,$date,$status" >> data/penghuni.csv
+
+            echo "penghuni '$nama' berhasil ditambahkan ke Kamar $kamar dengan status $status "
+
+            while true; do
+                read -p "Enter untuk kembali..." dummy
+                if [ "$dummy" != "" ]; then
+                    echo "Nope, harus enter"
+                else
+                    break   
+                fi
+            done
+        return
+    else
+          echo "Harus ketik aktif atau menunggak yakk"
+          continue
+     fi
+done
+}
+
+delete() {
+
+    echo "==================================================== "
+    echo "           『Delete Huni Kost Slebew 』              "
+    echo "==================================================== "
+
+    cp data/penghuni.csv sampah/history_hapus.csv
+    read -p "Masukkan nama penghuni yang ingin dihapus: " nama
+
+    if grep -q -w "$nama" sampah/history_hapus.csv 2>/dev/null; then
+        sed -i "/^$nama,/d" sampah/history_hapus.csv
+        echo "Penghuni '$nama' berhasil dihapus."
+    else
+        echo "Penghuni '$nama' tidak ditemukan."
+    fi
+
+     while true; do
+          read -p "Enter untuk kembali..." dummy
+          if [ "$dummy" != "" ]; then
+               echo "Nope, harus enter"
+
+               else
+                    break   
+          fi
+    done
+
+    return
+}
+
+list_huni() {
+    echo "==================================================== "
+    echo "             『List huni Kost Slebew 』              "
+    echo "==================================================== "
+    echo " NO | NAMA HUNI KOST | KAMAR | HARGA SEWA | STATUS | "
+    echo "---------------------------------------------------- "
+
+    awk -F',' '
+    BEGIN { 
+        aktif = 0
+        nunggak = 0
+        no = 1 
+    }
+    NR > 1 { 
+        
+        if ($5 == "aktif") aktif++
+        if ($5 == "menunggak") nunggak++
+        
+        tampilan = $0
+        gsub(/,/, " | ", tampilan)
+        
+        print no ". " tampilan
+        print "---------------------------------------------------- "
+        no++
+    }
+    END {
+
+        total = no - 1
+        print "Total: " total " | Aktif: " aktif " | Menunggak: " nunggak
+    }' data/penghuni.csv 2>/dev/null
+
+    echo "==================================================== "
+    read -p "Enter untuk kembali..." dummy
+    return
+}
+
+update_huni() {
+
+    echo "==================================================== "
+    echo "             『Update Huni Kost Slebew 』            "
+    echo "==================================================== "
+    read -p "Masukkan nama penghuni yang ingin diubah statusnya: " nama
+
+        if grep -q -w "$nama" data/penghuni.csv 2>/dev/null; then
+            read -p "Masukkan status Baru (Aktif atau nunggak): " stat_bar
+            sed -i "s/^\($nama,[^,]*,[^,]*,[^,]*\),.*/\1,$stat_bar/" data/penghuni.csv
+            echo "Penghuni '$nama' berhasil diganti menjadi $stat_bar"
+        else
+            echo "Penghuni '$nama' tidak ditemukan."
+        fi
+}
+
+laporan() {
+    rekap=$(awk -F',' '
+    BEGIN {
+        pendapatan = 0
+        pendapatan_nung = 0
+        jml_nunggak = 0
+        list_nunggak = ""
+    }
+    NR > 1 { 
+        
+        if ($5 == "aktif") {
+            pendapatan += $3
+        } 
+        else if ($5 == "menunggak") {
+            pendapatan_nung += $3
+            jml_nunggak++
+            list_nunggak = list_nunggak $1 ", "
+        }
+    }
+    END {
+
+        if (list_nunggak != "") {
+            sub(/, $/, "", list_nunggak)
+        } else {
+            list_nunggak = "-"
+        }
+
+        print "===================================================="
+        print "          『 Laporan huni Kost Slebew 』            "
+        print "===================================================="
+        print "Total Pendapatan Aktif : Rp " pendapatan
+        print "Total tunggakan        : RP " pendapatan_nung
+        print "Jumlah Orang Nunggak   : " jml_nunggak
+        print "Daftar Nama Nunggak    : " list_nunggak
+        print "===================================================="
+    }' data/penghuni.csv 2>/dev/null)
+
+    echo "$rekap"
+    
+    echo "$rekap" >> rekap/laporan_bulanan.txt
+    echo "Hasil rekapan sudah dimasukkan ke dalam rekap/laporan_bulanan.txt"
+    
+    read -p "Enter untuk kembali..." dummy
+}
+
+pengingat() {
+    while true; do
+        echo "==================================================== "
+        echo "                  『 Menu Kron 』                   "
+        echo "==================================================== "
+        echo "1. Lihat Jadwal Aktif (System)"
+        echo "2. Tambah Jadwal Baru"
+        echo "3. Hapus Semua Jadwal"
+        echo "4. Kembali"
+        echo "==================================================== "
+        read -p "Pilih 1-4: " pilih
+
+        if [ "$pilih" == "1" ]; then
+            echo "--- List Cron yang sedang berjalan ---"
+            crontab -l 2>/dev/null || echo "Belum ada jadwal aktif."
+            
+        elif [ "$pilih" == "2" ]; then
+            echo "Format: menit jam tgl bulan hari_minggu"
+            read -p "Input jadwal (contoh: 00 23 * * *): " jadwal
+            read -p "Perintah/Script yg dijalankan (path lengkap): " cmd
+            
+            (crontab -l 2>/dev/null; echo "$jadwal $cmd") | crontab -
+            
+            echo "Jadwal berhasil diaktifkan!"
+            echo "[$(date)] Berhasil input: $jadwal $cmd" >> log/tagihan.log
+
+        elif [ "$pilih" == "3" ]; then
+
+            crontab -r
+            echo "Semua jadwal cron telah dihapus!"
+            echo "[$(date)] Menghapus semua cron" >> log/tagihan.log
+
+        elif [ "$pilih" == "4" ]; then
+            break 
+        else
+            echo "pilihan salah. coba lagi"
+        fi
+
+        read -p "Enter untuk kembali..." dummy
+                if [ "$dummy" != "" ]; then
+                    echo "Nope, harus enter"
+                else
+                    break   
+                fi
+    done
+}
+
+while true
+do
+cat << "EOF"
+
+ __                  __           .__        ___.                  
+|  | ______  _______/  |_    _____|  |   ____\_ |__   ______  _  __
+|  |/ /  _ \/  ___/\   __\  /  ___/  | _/ __ \| __ \_/ __ \ \/ \/ /
+|    <  <_> )___ \  |  |    \___ \|  |_\  ___/| \_\ \  ___/\     / 
+|__|_ \____/____  > |__|   /____  >____/\___  >___  /\___  >\/\_/  
+     \/         \/              \/          \/    \/     \/        
+
+EOF
+
+echo "============================================="
+echo "      『SISTEM MANAJEMEN KOST SLEBEW 』      "
+echo "============================================="
+echo " ID | OPTION "
+echo "---------------------------------------------"
+echo " 1 | Menambahkan penghuni baru ke daftar "
+echo " 2 | Menghapus penghuni dari daftar "
+echo " 3 | Menampilkan daftar penghuni "
+echo " 4 | Update status penghuni " 
+echo " 5 | Cetak laporan keuangan "
+echo " 6 | Kelola Cron (pengingat tagihan) "
+echo " 7 | Exit "
+echo "============================================="
+read -p "Silahkan pilih [1 - 7]:" opt
+
+    if [[ $opt =~ $re ]] ; then
+        if [ "$opt" == "7" ]; then
+            exit 0
+        elif [ "$opt" == "1" ]; then
+            create
+        elif [ "$opt" == "2" ]; then
+            delete
+        elif [ "$opt" == "3" ]; then
+            list_huni
+        elif [ "$opt" == "4" ]; then
+            update_huni
+        elif [ "$opt" == "5" ]; then
+            laporan
+        elif [ "$opt" == "6" ]; then
+            pengingat
+        else 
+            echo "Harus memilih angka 1 - 7 yak!"
+            sleep 1
+            continue
+        fi
+    else 
+    echo "Harus masukin angka :<"
+    sleep 1
+    continue
+    fi
+done
